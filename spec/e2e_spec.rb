c1577991c207f61a6b0c0c203ce9f66b63a44a56
suzuki86 <tsnr0001@gmail.com>
2014-11-03 17:45:35 +0900

    First commit

---
+++ spec/e2e_spec.rb
@@ -0,0 +1,22 @@
+# encoding: utf-8
+
+require 'watir-webdriver'
+require 'headless'
+
+describe "qiita.com" do
+  before do
+    @headless = Headless.new
+    @headless.start
+    @browser = Watir::Browser.new :ff
+  end
+  it "has valid title" do
+    @browser.goto "http://qiita.com"
+    expect(@browser.title).to eq("Qiita - A technical knowledge sharing platform for programmers.")
+  end
+  after do
+    @browser.quit
+    @headless.destroy
+  end
+end
+
+
